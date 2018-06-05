c0550f6283714493df1cb5ceaab85076be167a38
Heather You <heatherhyou@gmail.com>
2018-06-05 16:37:23 -0400

    fix null error on custom attribute writer

--- weather-to-wear-api/app/models/user_city.rb
+++ weather-to-wear-api/app/models/user_city.rb
@@ -1,11 +1,16 @@
+require 'pry'
+
 class UserCity < ApplicationRecord
   belongs_to :user
   belongs_to :city
 
   def city_attributes=(city_attributes)
-    city_attributes.each do |index, city_attribute|
-      city_zip = City.find_or_create_by(zip_code: city_attribute)
-      self.city = city_zip
+    # binding.pry
+    city_attributes.each do |city_attribute_hash|
+      city_zip = city_attribute_hash[:zip_code]
+      # binding.pry
+      subject_city = City.find_or_create_by(zip_code: city_zip)
+      self.city = subject_city
     end
   end
 end
