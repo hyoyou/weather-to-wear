2893e72419e990b088e2bebff8abb37c9cd7e6a3
Heather You <heatherhyou@gmail.com>
2018-06-06 16:41:42 -0400

    render suggestions based on user data

--- weather-to-wear-client/src/components/Jacket.js
+++ weather-to-wear-client/src/components/Jacket.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class Jacket extends Component {
+const isLoggedIn = localStorage.getItem('Token') ? true : false;
+
+class Jacket extends Component {
   render() {
     const temperature = this.props.temperature;
     return (
       <div>
         <h2>Jacket Recommended?</h2>
-        {temperature < 55 ? <h1>YES</h1> : <h1>NO</h1>}
+        { isLoggedIn && this.props.cold ?
+          temperature < 60 ? <h1>YES</h1> : <h1>NO</h1>
+          :
+          temperature < 55 ? <h1>YES</h1> : <h1>NO</h1>
+        }
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    cold: state.session.user.cold_sensitivity
+  }
+}
+
+export default connect(mapStateToProps)(Jacket);
--- weather-to-wear-client/src/components/Umbrella.js
+++ weather-to-wear-client/src/components/Umbrella.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class Umbrella extends Component {
+const isLoggedIn = localStorage.getItem('Token') ? true : false;
+
+class Umbrella extends Component {
   render() {
     const precipitation = this.props.precipitation
     return (
       <div>
         <h2>Umbrella Recommended?</h2>
-        {precipitation > 50 ? <h1>YES</h1> : <h1>NO</h1>}
+        { isLoggedIn && this.props.handsfree ?
+          precipitation > 55 ? <h1>YES</h1> : <h1>NO</h1>
+          :
+          precipitation > 50 ? <h1>YES</h1> : <h1>NO</h1>
+        }
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    handsfree: state.session.user.opts_hands_free
+  }
+}
+
+export default connect(mapStateToProps)(Umbrella);
