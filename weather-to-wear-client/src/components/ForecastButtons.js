a8537d7aa6cc51b969cf4a7d2dd3e44ea8a1e374
Heather You <heatherhyou@gmail.com>
2018-06-07 12:36:25 -0400

    add key to each button to mute error

--- weather-to-wear-client/src/components/ForecastButtons.js
+++ weather-to-wear-client/src/components/ForecastButtons.js
@@ -4,7 +4,7 @@ export default class ForecastButtons extends Component {
   render() {
     return this.props.cities.map(city => {
       return (
-        <button className="btn btn-primary" style={{ marginRight: '10px' }} type="button" onClick={(event) => this.props.onClick(city, event)}>{city}</button>
+        <button key={city} className="btn btn-primary" style={{ marginRight: '10px' }} type="button" onClick={(event) => this.props.onClick(city, event)}>{city}</button>
       )
     })
   }
