abbd65c33ef92d13ee37b78b1cdcea36dedd5028
Heather You <heatherhyou@gmail.com>
2018-06-08 15:00:39 -0400

    add validation for format of 5 digits, but non-valid zip codes of 5 digits will still save

--- weather-to-wear-api/app/models/city.rb
+++ weather-to-wear-api/app/models/city.rb
@@ -2,7 +2,7 @@ class City < ApplicationRecord
   has_many :user_cities
   has_many :users, through: :user_cities
 
-  validates :zip_code,
-            presence: true,
-            uniqueness: true
+  validates :zip_code, presence: true,
+                       uniqueness: true,
+                       format: { with: /\A\d{5}\z/, message: "only allows numbers"}
 end
