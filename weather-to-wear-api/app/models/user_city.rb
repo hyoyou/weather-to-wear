ccb4d6c6a6b5d55fcef3b4f130efbab09cd0e410
Heather You <heatherhyou@gmail.com>
2018-06-11 15:03:52 -0400

    remove comments

--- weather-to-wear-api/app/models/user_city.rb
+++ weather-to-wear-api/app/models/user_city.rb
@@ -1,15 +1,10 @@
-require 'pry'
-
 class UserCity < ApplicationRecord
   belongs_to :user
   belongs_to :city
 
-  # having below validation blocks user from saving same zip code more than once,
-  # but it also doesn't let user add the same zip code once deleted
   # validates :city_id, uniqueness: true
 
   def city_attributes=(city_attributes)
-    # binding.pry
     city_zip = city_attributes[:zip_code]
     subject_city = City.find_or_create_by(zip_code: city_zip)
     self.city = subject_city
