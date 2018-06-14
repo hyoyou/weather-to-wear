419938f95a31d989fd0e13e10e69dfc2283869d8
Heather You <heatherhyou@gmail.com>
2018-06-14 13:50:00 -0400

    Convert to functional component

--- weather-to-wear-client/src/components/ForecastDetail.js
+++ weather-to-wear-client/src/components/ForecastDetail.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Jacket from './Jacket';
 import Umbrella from './Umbrella';
 
-export default class ForecastDetail extends Component {
-  render() {
-    return (
-      <div className="container py-4">
-        <div className="row">
-          <div className="col-md">
-            {this.props.date ? <h2>{this.props.date}</h2> : <h2>Forecast for {this.props.zipcode}</h2>}
-            <img src={this.props.icon} width='100' alt="icon for weather condition"/>
-            <p>Condition: {this.props.condition}</p>
-            <p>High Temperature: {this.props.high_temperature}&#8457;</p>
-            <p>Low Temperature: {this.props.low_temperature}&#8457;</p>
-            <p>% Precipitation: {this.props.precipitation}%</p>
-          </div>
+const ForecastDetail = (props) => {
+  return (
+    <div className="container py-4">
+      <div className="row">
+        <div className="col-md">
+          {props.date ? <h2>{props.date}</h2> : <h2>Forecast for {props.zipcode}</h2>}
+          <img src={props.icon} width='100' alt="icon for weather condition"/>
+          <p>Condition: {props.condition}</p>
+          <p>High Temperature: {props.high_temperature}&#8457;</p>
+          <p>Low Temperature: {props.low_temperature}&#8457;</p>
+          <p>% Precipitation: {props.precipitation}%</p>
+        </div>
 
-          <div className="col-md">
-            <Jacket temperature={this.props.low_temperature} />
-            <Umbrella precipitation={this.props.precipitation} />
-          </div>
+        <div className="col-md">
+          <Jacket temperature={props.low_temperature} />
+          <Umbrella precipitation={props.precipitation} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ForecastDetail;
