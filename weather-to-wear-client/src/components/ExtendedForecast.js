3777e7b5f0e1a0d16a5507dd8ff7cf043d55b756
Heather You <heatherhyou@gmail.com>
2018-06-14 13:45:38 -0400

    Convert to functional component

--- weather-to-wear-client/src/components/ExtendedForecast.js
+++ weather-to-wear-client/src/components/ExtendedForecast.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ForecastDetail from './ForecastDetail';
 
-class ExtendedForecast extends Component {
-  render() {
-    const { forecasts, zipcode } = this.props;
-    
-    return(
-      <div>
-        {forecasts && forecasts.map((forecast, index)=>
-          <ForecastDetail
-            key={index}
-            date={forecast.date.weekday}
-            zipcode={zipcode}
-            icon={forecast.icon_url}
-            condition={forecast.conditions}
-            high_temperature={forecast.high.fahrenheit}
-            low_temperature={forecast.low.fahrenheit}
-            precipitation={forecast.pop}
-          />
-        )}
-      </div>
-    )
-  }
+const ExtendedForecast = ({ forecasts, zipcode }) => {
+  return(
+    <div>
+      {forecasts && forecasts.map((forecast, index)=>
+        <ForecastDetail
+          key={index}
+          date={forecast.date.weekday}
+          zipcode={zipcode}
+          icon={forecast.icon_url}
+          condition={forecast.conditions}
+          high_temperature={forecast.high.fahrenheit}
+          low_temperature={forecast.low.fahrenheit}
+          precipitation={forecast.pop}
+        />
+      )}
+    </div>
+  )
 }
 
 export default ExtendedForecast;
